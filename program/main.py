6ee263ea69652ebaf38b9be3bcb5aadc294f8e80
Ken <imsorry1121@gmail.com>
2015-06-27 20:07:30 +0800

    finish output

--- program/main.py
+++ program/main.py
@@ -4,35 +4,54 @@ fmt = "%Y-%m-%dT%H:%M:%S"
 fmt2 = "%Y-%m-%d"
 
 def main(outputFile="../result/time.csv",logFile="../data/train/log_train_short.csv", enrollFile="../data/train/enrollment_train.csv", gtFile="../data/train/truth_train.csv"):
-	courseSTime = readCourse()
+	courseSTime = readCourseTime()
 	gts = readGT(gtFile)
+	eData = readEnrollData(enrollFile)
+	outputTimeSeries(courseSTime, gts, eData, logFile, outputFile)
+
+	# enrollment_id,username,course_id,time,source,event,object
+	# input
+	
+
+
+
+def outputTimeSeries(courseSTime, gts, eData, logFile, outputFile):
 	eids = list()
 	eSeries = dict()
-	eData = dict()
-# enrollment_id,username,course_id,time,source,event,object
-# input
 	with open(logFile, 'r') as fi:
 		titles = fi.readline().strip().split(',')
 		for line in fi:
-			[eid,uid,cid,time,source,event,obj] = line.strip().split(',')
+			[eid,time,source,event,obj] = line.strip().split(',')
 			t = datetime.strptime(time, fmt)
+			cid = eData[eid][1]
 			tdiff = (t-courseSTime[cid]).days
 			if eid not in eids:
 				eid = str(eid)
 				eids.append(eid)
 				eSeries[eid] = [0]*30
-				eData[eid] = [uid, cid]
 			eSeries[eid][tdiff] = eSeries[eid][tdiff]+1
-# output
+	# output
+	outputFileDrop = "../result/timeDrop.csv"
+	outputFileRemain = "../result/timeRemain.csv"
+	# outputFileRatioDrop = "../result/timeRatioDrop.csv"
+	# outputFileRatioRemain = "../result/timeRatioRemain.csv"
+	fo2 = open(outputFileDrop, 'w')
+	fo3 = open(outputFileRemain, 'w')
 	with open(outputFile,'w') as fo:
-		fo.write("gt,eid,uid,cid"+','.join([str(i) for i in range(30)])+'\n')
+		fo.write("gt,eid,uid,cid,"+','.join([str(i) for i in range(30)])+'\n')
 		for eid in eids:
 			gt = gts[eid]
-			outputStr = str(gt)+','+','.join([str(i) for i in (eData[eid]+eSeries[eid])])+'\n'
+			outputStr = str(gt)+','+eid+','+','.join([str(i) for i in (eData[eid]+eSeries[eid])])+'\n'
 			fo.write(outputStr)
+			if gt == '0':
+				fo3.write(outputStr)
+			else:
+				fo2.write(outputStr)
+	fo2.close()
+	fo3.close()
 
 
-def readCourse(courseFile="../data/date.csv"):
+def readCourseTime(courseFile="../data/date.csv"):
 	courseTime = dict()
 	with open(courseFile, 'r') as fi:
 		fi.readline()
@@ -48,6 +67,16 @@ def readGT(gtFile):
 			[eid, gt]=line.strip().split(',')
 			enrollGT[eid] = gt
 	return enrollGT
+
+def readEnrollData(enrollFile):
+	eData = dict()
+	with open(enrollFile, 'r') as fi:
+		fi.readline()
+		for line in fi:
+			[eid, uid, cid] = line.strip().split(',')
+			eData[eid] = [uid, cid]
+	return eData
+
 
 if __name__=="__main__":
 	main()
